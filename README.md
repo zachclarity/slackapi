5a5cca3c00e806dc2929d813cc21fe13d2aa3c0b
zach lewis <zlewis@clarityinnovates.com>
2022-01-19 13:43:58 -0600

    sample bot

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# slackapi
+# slackapi demo support Bot.
+
+The support bot initial template is below
+
+Key tools used.
+
+yarn install
+yarn dev
+
+Open a separate window and run ngrok for testing.
+
+https://ngrok.com/
+
+ngrok http 3000
+
+
---
+++ app.js
@@ -0,0 +1,28 @@
+const { App } = require("@slack/bolt");
+require("dotenv").config();
+// Initializes your app with your bot token and signing secret
+const app = new App({
+    token: process.env.SLACK_APP_TOKEN,
+    signingSecret: process.env.SLACK_SIGNING_SECRET,
+});
+
+app.event('message', async ({ event, client, context }) => {
+    try {
+        if ( event.channel === "C02U4BEJ1HD") {
+            console.log("from:(" +`<@${event.user}>`+ ") Msg:(" + event.text + ")")
+            const result = await client.chat.postMessage({
+                channel: "C02U4BEJ1HD",
+                text: `Welcome to SRE Support Answers Bot, <@${event.user}>! 🎉 We will have an answer for you in a few minutes.`          });
+        }
+    } catch (error) {
+        console.log(error)
+    }
+});
+
+
+(async () => {
+    const port = 3000
+    // Start your app
+    await app.start(process.env.PORT || port);
+    console.log(`⚡️ Slack Bolt app is running on port ${port}!`);
+})();
